2e795016849446226ea1086137550c5c1ce22af2
Washington <washingtoncampos9@gmail.com>
2021-02-13 15:39:08 -0300

    feat: add globalStyles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import GlobalStyles from '../styles/globalStyles';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
