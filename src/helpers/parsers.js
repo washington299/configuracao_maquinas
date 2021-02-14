e70b9d7e1a847a9a72abaf6ba22266324f9179f4
Washington <washingtoncampos9@gmail.com>
2021-02-14 17:51:08 -0300

    feat: create formatSlugToString parser function

--- src/helpers/parsers.js
+++ src/helpers/parsers.js
@@ -2,3 +2,9 @@ export const createSlugBasedOnString = (value) => {
 	const slug = value.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replaceAll(' ', '-');
 	return slug;
 };
+
+export const formatSlugToString = (value) => {
+	console.log(value);
+	const slug = value.replaceAll('-', ' ');
+	return slug;
+};
