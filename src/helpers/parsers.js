fa23879f72d1f8b03731efbd1afa9a080cf72327
washington <washingtoncampos9@gmail.com>
2021-02-27 19:14:10 -0300

    fix: fix error on replace function

--- src/helpers/parsers.js
+++ src/helpers/parsers.js
@@ -2,11 +2,11 @@ export const createSlugBasedOnString = value => {
 	const slug = value
 		.normalize('NFD')
 		.replace(/[\u0300-\u036f]/g, '')
-		.replaceAll(' ', '-');
+		.replace(/  +/g, ' ');
 	return slug;
 };
 
 export const formatSlugToString = value => {
-	const slug = value.replaceAll('-', ' ');
+	const slug = value.normalize('NFD').replaceAll('-', ' ');
 	return slug;
 };
