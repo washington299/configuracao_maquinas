cf7e170d71c7449f57f65c29e7d30b210a7511b3
Washington Campos <washingtoncampos9@gmail.com>
2022-08-27 18:08:32 -0300

    chore: set up container width

---
+++ src/styles/container.js
@@ -0,0 +1,5 @@
+export const Container = {
+	baseStyle: {
+		maxWidth: '1080px',
+	},
+};
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+import { Container } from './container';
+
+const theme = extendTheme({
+	components: {
+		Container
+	},
+});
+
+export default theme;
