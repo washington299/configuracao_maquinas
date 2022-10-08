77c0800ba2fe96b22e3ff96209a3b5e4c8b25c1f
Washington Campos <washingtoncampos9@gmail.com>
2022-10-08 18:00:22 -0300

    test: ensure modal form works correctly

---
+++ src/components/AddSettingsModal/test.js
@@ -0,0 +1,42 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { AddSettingsModal } from '.';
+
+describe('<AddSettingsModal />', () => {
+	it('Should render input with correct value', () => {
+		render(<AddSettingsModal isOpen={true} />);
+
+		const input = screen.getByLabelText(/Nome da configuração/i);
+		const text = 'Test';
+
+		fireEvent.change(input, { target: { value: text } });
+
+		expect(input).toHaveValue(text);
+	});
+
+	it('Should show error message when user try to create with empty name', () => {
+		render(<AddSettingsModal isOpen={true} />);
+
+		const input = screen.getByLabelText(/Nome da configuração/i);
+		const createButton = screen.getByRole('button', { name: /Criar/i });
+
+		fireEvent.change(input, { target: { value: '' } });
+		fireEvent.click(createButton);
+
+		expect(screen.getByText(/Nome é obrigatório/i)).toBeInTheDocument();
+	});
+
+	it('Should dispatch onClose function when setting is created correctly', () => {
+		const mockOnClose = jest.fn();
+
+		render(<AddSettingsModal isOpen={true} onClose={mockOnClose} />);
+
+		const input = screen.getByLabelText(/Nome da configuração/i);
+		const createButton = screen.getByRole('button', { name: /Criar/i });
+
+		fireEvent.change(input, { target: { value: 'Test' } });
+		fireEvent.click(createButton);
+
+		expect(mockOnClose).toHaveBeenCalledTimes(1);
+	});
+});
