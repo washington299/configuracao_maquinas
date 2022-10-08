7eb826c0f4d94a8e85252c4906d305451c8d4cd4
Washington Campos <washingtoncampos9@gmail.com>
2022-10-08 00:03:04 -0300

    feat: add letter and settingsQtd props to MachineCard component

--- src/components/MachineCard/index.js
+++ src/components/MachineCard/index.js
@@ -2,7 +2,7 @@ import { Heading, VStack, Text, Icon } from '@chakra-ui/react';
 
 import { RobotIcon } from 'Icons/RobotIcon';
 
-export const MachineCard = () => {
+export const MachineCard = ({ letter, settingsQtd }) => {
 	return (
 		<VStack
 			boxShadow="base"
@@ -14,8 +14,8 @@ export const MachineCard = () => {
 			<Icon width="full" height="150" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg">
 				<RobotIcon />
 			</Icon>
-			<Heading as="h2" fontSize="2xl">Máquina A</Heading>
-			<Text>Quantidade de configurações: 4</Text>
+			<Heading as="h2" fontSize="2xl">Máquina {letter}</Heading>
+			<Text>Quantidade de configurações: {settingsQtd}</Text>
 		</VStack>
 	);
 };
