21c5a5672122fb222a1c0decf2ce0152cb151bfe
Washington Campos <washingtoncampos9@gmail.com>
2022-10-08 00:54:01 -0300

    feat: add link to MachineCard

--- src/components/MachineCard/index.js
+++ src/components/MachineCard/index.js
@@ -1,21 +1,24 @@
+import Link from 'next/link';
 import { Heading, VStack, Text, Icon } from '@chakra-ui/react';
 
 import { RobotIcon } from 'Icons/RobotIcon';
 
 export const MachineCard = ({ letter, settingsQtd }) => {
 	return (
-		<VStack
-			boxShadow="base"
-			borderRadius="md"
-			p={3}
-			cursor="pointer"
-			_hover={{ bg: 'blackAlpha.50' }}
-		>
-			<Icon width="full" height="150" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg">
-				<RobotIcon />
-			</Icon>
-			<Heading as="h2" fontSize="2xl">Máquina {letter}</Heading>
-			<Text>Quantidade de configurações: {settingsQtd}</Text>
-		</VStack>
+		<Link href={`/machine/${letter}`}>
+			<VStack
+				boxShadow="base"
+				borderRadius="md"
+				p={3}
+				cursor="pointer"
+				_hover={{ bg: 'blackAlpha.50' }}
+			>
+				<Icon width="full" height="150" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg">
+					<RobotIcon />
+				</Icon>
+				<Heading as="h2" fontSize="2xl">Máquina {letter}</Heading>
+				<Text>Quantidade de configurações: {settingsQtd}</Text>
+			</VStack>
+		</Link>
 	);
 };
