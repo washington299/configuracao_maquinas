561102d0d3bbe0269c9905f1facad5f33b501e0e
Washington Campos <washingtoncampos9@gmail.com>
2022-10-08 01:22:15 -0300

    chore: add react-query

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import theme from '../styles/theme';
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }) {
 	return (
-		<ChakraProvider theme={theme}>
-			<Component {...pageProps} />
-		</ChakraProvider>
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider theme={theme}>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</QueryClientProvider>
 	);
-}
+};
 
 export default MyApp;
