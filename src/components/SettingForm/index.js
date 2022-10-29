54a62a0926847544aaf7c503959376fdd7832414
Washington Campos <washingtoncampos9@gmail.com>
2022-10-29 14:29:57 -0300

    feat: add formData state to handle form data

--- src/components/SettingForm/index.js
+++ src/components/SettingForm/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import {
 	Heading,
@@ -25,7 +26,13 @@ import { DeleteSettingModal } from 'components/DeleteSettingModal';
 export const SettingForm = () => {
 	const router = useRouter();
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+	const [formData, setFormData] = useState({});
+
+	const handleInputFormDataChange = (e) => {
+		setFormData(previousData => ({ ...previousData, [e.target.name]: e.target.value }));
+	};
 
 	return (
 		<>
@@ -34,7 +41,12 @@ export const SettingForm = () => {
 			<VStack w="full" spacing={6} mt={6}>
 				<FormControl>
 					<FormLabel mb={0}>Diâmetro externo (mm):</FormLabel>
-					<NumberInput min={0}>
+					<NumberInput
+						min={0}
+						name="diametro"
+						value={formData.diametro}
+						onChange={(value) => setFormData(previousData => ({ ...previousData, diametro: value }))}
+					>
 						<NumberInputField />
 						<NumberInputStepper>
 							<NumberIncrementStepper />
@@ -46,7 +58,10 @@ export const SettingForm = () => {
 				<VStack w="full" alignItems="normal" spacing={0}>
 					<Text>Material</Text>
 					<Box w="full" border="1px solid" borderColor="gray.300" p={6}>
-						<CheckboxGroup>
+						<CheckboxGroup
+							value={formData.materialCheck}
+							onChange={(value) => setFormData(previousData => ({ ...previousData, materialCheck: value }))}
+						>
 							<SimpleGrid columns={[3, 4, 5, 6]} spacing={4}>
 								<Checkbox value="PPN">PPN</Checkbox>
 								<Checkbox value="PPP">PPP</Checkbox>
@@ -64,32 +79,32 @@ export const SettingForm = () => {
 				<SimpleGrid w="full" columns={[1, 2, 3]} spacing={6}>
 					<FormControl>
 						<FormLabel mb={0}>Zona 1</FormLabel>
-						<Input />
+						<Input name="zona1" value={formData.zona1} onChange={handleInputFormDataChange} />
 					</FormControl>
 
 					<FormControl>
 						<FormLabel mb={0}>Zona 2</FormLabel>
-						<Input />
+						<Input name="zona2" value={formData.zona2} onChange={handleInputFormDataChange} />
 					</FormControl>
 
 					<FormControl>
 						<FormLabel mb={0}>Zona 3</FormLabel>
-						<Input />
+						<Input name="zona3" value={formData.zona3} onChange={handleInputFormDataChange} />
 					</FormControl>
 
 					<FormControl>
 						<FormLabel mb={0}>Zona 4</FormLabel>
-						<Input />
+						<Input name="zona4" value={formData.zona4} onChange={handleInputFormDataChange} />
 					</FormControl>
 
 					<FormControl>
 						<FormLabel mb={0}>Zona 5</FormLabel>
-						<Input />
+						<Input name="zona5" value={formData.zona5} onChange={handleInputFormDataChange} />
 					</FormControl>
 
 					<FormControl>
 						<FormLabel mb={0}>Zona 6</FormLabel>
-						<Input />
+						<Input name="zona6" value={formData.zona6} onChange={handleInputFormDataChange} />
 					</FormControl>
 				</SimpleGrid>
 
@@ -98,32 +113,56 @@ export const SettingForm = () => {
 				<SimpleGrid w="full" columns={[1, 2]} spacing={6}>
 					<FormControl>
 						<FormLabel mb={0}>Velocidade da rosca  (rpm)</FormLabel>
-						<Input />
+						<Input
+							name="velocidade_da_rosca"
+							value={formData.velocidade_da_rosca}
+							onChange={handleInputFormDataChange}
+						/>
 					</FormControl>
 
 					<FormControl>
 						<FormLabel mb={0}>Amperagem da rosca  (A)</FormLabel>
-						<Input />
+						<Input
+							name="amperagem_da_rosca"
+							value={formData.amperagem_da_rosca}
+							onChange={handleInputFormDataChange}
+						/>
 					</FormControl>
 
 					<FormControl>
 						<FormLabel mb={0}>Pressão do material na saída da rosca  (bar)</FormLabel>
-						<Input />
+						<Input
+							name="pressao_do_material_na_saida_da_rosca"
+							value={formData.pressao_do_material_na_saida_da_rosca}
+							onChange={handleInputFormDataChange}
+						/>
 					</FormControl>
 
 					<FormControl>
 						<FormLabel mb={0}>Pressão do freio  (bar)</FormLabel>
-						<Input />
+						<Input
+							name="pressao_do_freio"
+							value={formData.pressao_do_freio}
+							onChange={handleInputFormDataChange}
+						/>
 					</FormControl>
 
 					<FormControl>
 						<FormLabel mb={0}>Temperatura da água  (°c)</FormLabel>
-						<Input />
+						<Input
+							name="temperatura_da_agua"
+							value={formData.temperatura_da_agua}
+							onChange={handleInputFormDataChange}
+						/>
 					</FormControl>
 
 					<FormControl>
 						<FormLabel mb={0}>Produção  (kg/hora)</FormLabel>
-						<Input />
+						<Input
+							name="producao"
+							value={formData.producao}
+							onChange={handleInputFormDataChange}
+						/>
 					</FormControl>
 				</SimpleGrid>
 			</VStack>
