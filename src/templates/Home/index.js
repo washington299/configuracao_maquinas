50fcd5a1b1670516732e16e41ad35caa45f68431
Washington Campos <washingtoncampos9@gmail.com>
2022-11-24 22:07:36 -0300

    feat: create HomeTemplate component

---
+++ src/templates/Home/index.js
@@ -0,0 +1,20 @@
+import { Container, SimpleGrid } from '@chakra-ui/react';
+
+import { MachineCard } from 'components/MachineCard';
+import { Title } from 'components/Title';
+
+import { machinesList } from 'const/machines';
+
+export const HomeTemplate = () => {
+	return (
+		<Container py={10}>
+			<Title text="Ficha referencial de preparação e regulagem extrusora tarugos" />
+
+			<SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={8}>
+				{machinesList.map((machineLetter) => (
+					<MachineCard key={machineLetter} letter={machineLetter} />
+				))}
+			</SimpleGrid>
+		</Container>
+	);
+};
