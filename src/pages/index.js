36ce0b6955fe626ce6a5089d13d90e80e793aedf
Washington Campos <washingtoncampos9@gmail.com>
2022-11-24 22:09:05 -0300

    refactor: add HomeTemplate to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,5 @@
-import { Container, SimpleGrid } from '@chakra-ui/react';
+import { HomeTemplate } from "templates/Home";
 
-import { MachineCard } from 'components/MachineCard';
-import { Title } from 'components/Title';
-
-import { machinesList } from 'const/machines';
-
-const Home = () => {
-	return (
-		<Container py={10}>
-			<Title text="Ficha referencial de preparação e regulagem extrusora tarugos" />
-
-			<SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={8}>
-				{machinesList.map((machineLetter) => (
-					<MachineCard key={machineLetter} letter={machineLetter} />
-				))}
-			</SimpleGrid>
-		</Container>
-	);
-};
+const Home = () => <HomeTemplate />;
 
 export default Home;
