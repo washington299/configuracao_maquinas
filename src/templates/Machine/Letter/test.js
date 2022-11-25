e7517e89f4e57242afa04126745c198a48f87708
Washington Campos <washingtoncampos9@gmail.com>
2022-11-24 23:00:26 -0300

    test: ensure all type of response from useGetMachineSettings show correct elements

--- src/templates/Machine/Letter/test.js
+++ src/templates/Machine/Letter/test.js
@@ -11,7 +11,19 @@ useRouter.mockImplementation(() => ({
 	push
 }));
 
+let isLoading = false;
+let data;
+const useGetMachineSettings = jest.spyOn(require('services/queries'), 'useGetMachineSettings');
+useGetMachineSettings.mockImplementation(() => ({
+	data,
+	isLoading,
+}));
+
 describe('<MachineLetterTemplate />', () => {
+	beforeEach(() => {
+		isLoading = false;
+	});
+
 	it('Should redirect to Home when arrow back is clicked', () => {
 		globalRender(<MachineLetterTemplate />);
 
@@ -27,5 +39,34 @@ describe('<MachineLetterTemplate />', () => {
 		globalRender(<MachineLetterTemplate />);
 
 		expect(screen.getByRole('heading', { name: /Máquina A lista de configurações/i })).toBeInTheDocument();
+	});
+
+	it('Should show a message when api returns empty data', () => {
+		globalRender(<MachineLetterTemplate />);
+
+		expect(screen.getByText(/Essa máquina ainda não possui configurações./i)).toBeInTheDocument();
+	});
+
+	it('Should show a Spinner if react-query isLoading is true', () => {
+		isLoading = true;
+
+		globalRender(<MachineLetterTemplate />);
+
+		expect(screen.getByTestId('Spinner')).toBeInTheDocument();
+	});
+
+	it('Should show correct quantity of settings based on api response', () => {
+		data = {
+			data: [
+				{ _id: 1, slug: 'slug-1' },
+				{ _id: 2, slug: 'slug-2' },
+				{ _id: 3, slug: 'slug-3' },
+			],
+		};
+
+		globalRender(<MachineLetterTemplate />);
+
+		expect(screen.getAllByRole('link')).toHaveLength(3);
+		expect(screen.getByText('slug-1')).toBeInTheDocument();
 	});
 });
