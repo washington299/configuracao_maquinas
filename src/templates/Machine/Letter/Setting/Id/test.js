ee3b80cff623141da15f359e47cd7bd5015d68f9
Washington Campos <washingtoncampos9@gmail.com>
2022-11-27 19:37:55 -0300

    test: ensure mutate is called with correct values

--- src/templates/Machine/Letter/Setting/Id/test.js
+++ src/templates/Machine/Letter/Setting/Id/test.js
@@ -11,6 +11,17 @@ useRouter.mockImplementation(() => ({
 	push,
 }));
 
+const mutate = jest.fn();
+const useAddMachineSettingsData = jest.spyOn(require('services/queries'), 'useAddMachineSettingsData');
+useAddMachineSettingsData.mockImplementation(() => ({
+	mutate,
+}));
+
+const useGetMachineSettingsData = jest.spyOn(require('services/queries'), 'useGetMachineSettingsData');
+useGetMachineSettingsData.mockImplementation(() => ({
+	isLoading: false,
+}));
+
 describe('<MachineLetterSettingId />', () => {
 	it('Should redirect Arrow back to correct route', () => {
 		globalRender(<MachineLetterSettingId />);
@@ -26,5 +37,34 @@ describe('<MachineLetterSettingId />', () => {
 		globalRender(<MachineLetterSettingId />);
 
 		expect(screen.getByRole('heading', { name: /Máquina A - 40 PPN/i }))
+	});
+
+	it('Should call mutate with correct values when form is submitted', () => {
+		globalRender(<MachineLetterSettingId />);
+
+		expect(mutate).not.toBeCalled();
+
+		fireEvent.change(screen.getByLabelText(/Diâmetro externo/i), { target: { value: '40 PPN' } });
+		fireEvent.click(screen.getByRole('button', { name: /Salvar/i }));
+
+		expect(mutate).toHaveBeenCalledWith({
+			"amperagem_da_rosca": "",
+      "diametro": "40 PPN",
+      "extrusora": "A",
+    	"materialCheck": "",
+      "name": "A",
+    	"slug": "40 PPN",
+			"pressao_do_freio": "",
+			"pressao_do_material_na_saida_da_rosca": "",
+			"producao": "",
+			"temperatura_da_agua": "",
+			"velocidade_da_rosca": "",
+			"zona1": "",
+			"zona2": "",
+			"zona3": "",
+			"zona4": "",
+			"zona5": "",
+			"zona6": "",
+		});
 	});
 });
