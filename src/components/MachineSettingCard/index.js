5ca61d458900f2e90279fa0b4bfd897da299f035
Washington Campos <washingtoncampos9@gmail.com>
2022-12-01 19:41:52 -0300

    feat: add DeleteSettingModal to MachineSettingcard and create a delete icon to delete setting

--- src/components/MachineSettingCard/index.js
+++ src/components/MachineSettingCard/index.js
@@ -1,18 +1,38 @@
-import { HStack, Text, Icon } from '@chakra-ui/react';
-import { Settings } from '@material-ui/icons';
+import NextLink from 'next/link';
+import { HStack, Text, Icon, LinkBox, LinkOverlay, Button, useDisclosure } from '@chakra-ui/react';
+import { Settings, Delete } from '@material-ui/icons';
 
-export const MachineSettingCard = ({ name }) => {
+import { DeleteSettingModal } from 'components/DeleteSettingModal';
+
+export const MachineSettingCard = ({ setting, path }) => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
 	return (
-		<HStack
-			alignItems="center"
-			p={3}
-			boxShadow="base"
-			borderRadius="md"
-			cursor="pointer"
-			_hover={{ bg: 'blackAlpha.50' }}
-		>
-			<Text>{name}</Text>
-			<Icon as={Settings} w="100px" h="100px" />
-		</HStack>
+		<>
+			<DeleteSettingModal isOpen={isOpen} onClose={onClose} settingId={setting._id} />
+
+			<LinkBox>
+				<HStack
+					alignItems="center"
+					justifyContent="space-between"
+					p={3}
+					boxShadow="base"
+					borderRadius="md"
+					cursor="pointer"
+					_hover={{ bg: 'blackAlpha.50' }}
+				>
+					<NextLink href={path} passHref>
+						<LinkOverlay flex={1} display="flex" alignItems="center">
+							<Text as="span">{setting.slug}</Text>
+							<Icon as={Settings} ml={1} />
+						</LinkOverlay>
+					</NextLink>
+
+					<Button minW="fit-content" h="fit-content" bg="none" p={0} onClick={onOpen}>
+						<Icon as={Delete} color="red" />
+					</Button>
+				</HStack>
+			</LinkBox>
+		</>
 	);
 };
