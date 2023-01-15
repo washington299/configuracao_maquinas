f076c6d302a5d1d6dd6b6230f56022e67a42c4e0
Washington Campos <washingtoncampos9@gmail.com>
2023-01-15 20:32:53 -0300

    refactor: change fields text color

--- src/components/SettingForm/index.js
+++ src/components/SettingForm/index.js
@@ -35,9 +35,17 @@ const initialFormState = {
 	producao: '',
 };
 
+const formFieldsTextColor = 'red.500';
+const formLabelStyles = {
+	mb: 0,
+	fontSize: ['xs', 'md'],
+	fontWeight: "normal",
+};
+
 export const SettingForm = ({ handleSubmitSettingForm }) => {
 	const router = useRouter();
 	const { create, letter, id } = router.query;
+
 
 	const { isLoading, data } = useGetMachineSettingsData(letter, id, create);
 
@@ -63,12 +71,13 @@ export const SettingForm = ({ handleSubmitSettingForm }) => {
 				<>
 					<VStack w="full" spacing={6} mt={6}>
 						<FormControl>
-							<FormLabel mb={0}>Diâmetro externo (mm):</FormLabel>
+							<FormLabel {...formLabelStyles}>Diâmetro externo (mm):</FormLabel>
 							<Input
 								name="diametro"
 								placeholder="Diametro"
 								value={formData.diametro}
 								onChange={handleInputFormDataChange}
+								color={formFieldsTextColor}
 							/>
 						</FormControl>
 
@@ -80,75 +89,81 @@ export const SettingForm = ({ handleSubmitSettingForm }) => {
 									onChange={(value) => setFormData(previousData => ({ ...previousData, materialCheck: value }))}
 								>
 									<SimpleGrid columns={[3, 4, 5, 6]} spacing={4}>
-										<Radio value="PPN">PPN</Radio>
-										<Radio value="PPP">PPP</Radio>
-										<Radio value="PPZ">PPZ</Radio>
-										<Radio value="PEADN">PEADN</Radio>
-										<Radio value="PEADP">PEADP</Radio>
-										<Radio value="OUTROS">OUTROS</Radio>
+										<Radio value="PPN" colorScheme="red">PPN</Radio>
+										<Radio value="PPP" colorScheme="red">PPP</Radio>
+										<Radio value="PPZ" colorScheme="red">PPZ</Radio>
+										<Radio value="PEADN" colorScheme="red">PEADN</Radio>
+										<Radio value="PEADP" colorScheme="red">PEADP</Radio>
+										<Radio value="OUTROS" colorScheme="red">OUTROS</Radio>
 									</SimpleGrid>
 								</RadioGroup>
 							</Box>
 						</VStack>
 
-						<Heading fontSize="xl">Temperaturas (°c)</Heading>
+						<Heading fontSize='xl'>Temperaturas (°c)</Heading>
 
 						<SimpleGrid w="full" columns={[1, 2, 3]} spacing={6}>
 							<FormControl>
-								<FormLabel mb={0}>Zona 1</FormLabel>
+								<FormLabel {...formLabelStyles}>Zona 1</FormLabel>
 								<Input
 									name="zona1"
 									placeholder="Zona 1"
+									color={formFieldsTextColor}
 									value={formData.zona1}
 									onChange={handleInputFormDataChange}
 								/>
 							</FormControl>
 
 							<FormControl>
-								<FormLabel mb={0}>Zona 2</FormLabel>
+								<FormLabel {...formLabelStyles}>Zona 2</FormLabel>
 								<Input
 									name="zona2"
 									placeholder="Zona 2"
+									color={formFieldsTextColor}
 									value={formData.zona2}
 									onChange={handleInputFormDataChange}
 								/>
 							</FormControl>
 
 							<FormControl>
-								<FormLabel mb={0}>Zona 3</FormLabel>
+								<FormLabel {...formLabelStyles}>Zona 3</FormLabel>
 								<Input
 									name="zona3"
 									placeholder="Zona 3"
+									color={formFieldsTextColor}
 									value={formData.zona3}
 									onChange={handleInputFormDataChange}
 								/>
 							</FormControl>
 
 							<FormControl>
-								<FormLabel mb={0}>Zona 4</FormLabel>
+								<FormLabel {...formLabelStyles}>Zona 4</FormLabel>
 								<Input
 									name="zona4"
 									placeholder="Zona 4"
+									color={formFieldsTextColor}
 									value={formData.zona4}
 									onChange={handleInputFormDataChange}
 								/>
 							</FormControl>
 
 							<FormControl>
-								<FormLabel mb={0}>Zona 5</FormLabel>
+								<FormLabel {...formLabelStyles}>Zona 5</FormLabel>
 								<Input
 									name="zona5"
 									placeholder="Zona 5"
+									color={formFieldsTextColor}
 									value={formData.zona5}
 									onChange={handleInputFormDataChange}
 								/>
 							</FormControl>
 
 							<FormControl>
-								<FormLabel mb={0}>Zona 6</FormLabel>
+								<FormLabel {...formLabelStyles}>Zona 6</FormLabel>
 								<Input
 									name="zona6"
 									placeholder="Zona 6"
+									color={formFieldsTextColor}
 									value={formData.zona6}
 									onChange={handleInputFormDataChange}
 								/>
@@ -159,60 +174,66 @@ export const SettingForm = ({ handleSubmitSettingForm }) => {
 
 						<SimpleGrid w="full" columns={[1, 2]} spacing={6}>
 							<FormControl>
-								<FormLabel mb={0}>Velocidade da rosca  (rpm)</FormLabel>
+								<FormLabel {...formLabelStyles}>Velocidade da rosca  (rpm)</FormLabel>
 								<Input
 									name="velocidade_da_rosca"
 									placeholder="Velocidade da rosca"
+									color={formFieldsTextColor}
 									value={formData.velocidade_da_rosca}
 									onChange={handleInputFormDataChange}
 								/>
 							</FormControl>
 
 							<FormControl>
-								<FormLabel mb={0}>Amperagem da rosca  (A)</FormLabel>
+								<FormLabel {...formLabelStyles}>Amperagem da rosca  (A)</FormLabel>
 								<Input
 									name="amperagem_da_rosca"
 									placeholder="Amperagem da rosca"
+									color={formFieldsTextColor}
 									value={formData.amperagem_da_rosca}
 									onChange={handleInputFormDataChange}
 								/>
 							</FormControl>
 
 							<FormControl>
-								<FormLabel mb={0}>Pressão do material na saída da rosca  (bar)</FormLabel>
+								<FormLabel {...formLabelStyles}>Pressão do material na saída da rosca  (bar)</FormLabel>
 								<Input
 									name="pressao_do_material_na_saida_da_rosca"
 									placeholder="Pressão do material na saida da rosca"
+									color={formFieldsTextColor}
 									value={formData.pressao_do_material_na_saida_da_rosca}
 									onChange={handleInputFormDataChange}
 								/>
 							</FormControl>
 
 							<FormControl>
-								<FormLabel mb={0}>Pressão do freio  (bar)</FormLabel>
+								<FormLabel {...formLabelStyles}>Pressão do freio  (bar)</FormLabel>
 								<Input
 									name="pressao_do_freio"
 									placeholder="Pressão do freio"
+									color={formFieldsTextColor}
 									value={formData.pressao_do_freio}
 									onChange={handleInputFormDataChange}
 								/>
 							</FormControl>
 
 							<FormControl>
-								<FormLabel mb={0}>Temperatura da água  (°c)</FormLabel>
+								<FormLabel {...formLabelStyles}>Temperatura da água  (°c)</FormLabel>
 								<Input
 									name="temperatura_da_agua"
 									placeholder="Temperatura da agua"
+									color={formFieldsTextColor}
 									value={formData.temperatura_da_agua}
 									onChange={handleInputFormDataChange}
 								/>
 							</FormControl>
 
 							<FormControl>
-								<FormLabel mb={0}>Produção  (kg/hora)</FormLabel>
+								<FormLabel {...formLabelStyles}>Produção  (kg/hora)</FormLabel>
 								<Input
 									name="producao"
 									placeholder="Produção"
+									color={formFieldsTextColor}
 									value={formData.producao}
 									onChange={handleInputFormDataChange}
 								/>
